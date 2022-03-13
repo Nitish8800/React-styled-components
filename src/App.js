7bd7d93ee5d906076233b205d71deca11a89c3eb
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-14 03:18:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./styles.css";
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 2rem;
+  border: 1px solid blueviolet;
+
+  h1 {
+    color: blue;
+  }
+
+  h2 {
+    color: red;
+  }
+`;
+
+const A = styled.a`
+  text-decoration: none;
+  font-size: 24px;
+`;
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Nitish</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Wrapper>
+        <A href="https://www.google.com/"> go to Google</A>
+        <h1>Hello Nitish</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </Wrapper>
     </div>
   );
 }
